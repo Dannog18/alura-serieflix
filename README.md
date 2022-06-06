bdab77986300b7369764a6864d60f8bc6cb3300d
Daniel <danielmacnog@gmail.com>
2022-06-06 12:24:39 -0300

    committing serieflix

---
+++ README.md
@@ -0,0 +1,4 @@
+# SérieFlix
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Dansan17/pen/QWaLMgb](https://codepen.io/Dansan17/pen/QWaLMgb).
+
---
+++ script.js
@@ -0,0 +1,69 @@
+var listaFilmes = [
+    "https://posterspy.com/wp-content/uploads/2019/12/main-2-1500x2250.png",
+    "<br>",
+    //
+    "https://th.bing.com/th/id/OIP.CdcvYKM8WroxC5beccHkaQHaK-?pid=ImgDet&rs=",
+    "<br>",
+    //
+    "https://th.bing.com/th/id/OIP.tusEn1xORzGa2-qx3BlgHAHaLH?pid=ImgDet&rs=1",
+    "<br>",
+  
+    "https://m.media-amazon.com/images/M/MV5BY2NmNzEzN2QtMTBhOC00OGY1LThkMGMtZTIwMTE3MmUyNDc1XkEyXkFqcGdeQXVyMTkxNjUyNQ@@.jpg",
+    "<br>",
+  
+    "https://art-s.nflximg.net/e9afc/b08ab393753a059827cb9923a322862b101e9afc.jpg",
+    "<br>",
+  
+    "https://th.bing.com/th/id/R.327502d36e9265d69099111ee0c4afa9?rik=kNQaHjU0TsNdIQ&riu=http%3a%2f%2fes.web.img3.acsta.net%2fpictures%2f15%2f11%2f20%2f12%2f35%2f130260.jpg&ehk=GUvV8ZGz7DHLAbq9mvXlxwXv2woyoc8ByAveE5TD8Uc%3d&risl=&pid=ImgRaw&r=0",
+    "<br>",
+  
+    "https://harmonylemag.com/wp-content/uploads/2020/09/The-Chosen-3-768x1024.jpg",
+    "<br>",
+  
+    "https://filmspot.com.pt/images/series/posters/big/96677.jpg",
+    "<br>",
+  
+    "https://th.bing.com/th/id/R.a9ae1a84c75c44e360494e89af7ee5fd?rik=H8%2bjYgGLP1de7A&riu=http%3a%2f%2fwx2.sinaimg.cn%2fmw690%2f00602RHagy1flkc04uluzj30go0m8jxe.jpg&ehk=oU689uajmewgKuvjv5AuA4bgLUO9niQmpL3V7bRHPCE%3d&risl=&pid=ImgRaw&r=0",
+    "<br>",
+    "https://th.bing.com/th/id/R.bc0b60250ac583f79dddd228a1fab63f?rik=N%2f9%2f6sz7dZdU5g&riu=http%3a%2f%2f3.bp.blogspot.com%2f-wdB3ZDaDzuk%2fVplJSroyFvI%2fAAAAAAAAFWs%2fW7eufQqMeXI%2fs1600%2fVikings_Temporada_02_dvd_Yumbo73.jpg&ehk=Jqfvos%2fpwh2E9RcTggFjeIkX3WfyrvA6Y%2bQgIIHdZgs%3d&risl=&pid=ImgRaw&r=0",
+    "<br>",
+    "https://i.pinimg.com/736x/b8/fb/80/b8fb80b272f78526095b383eb2374739.jpg",
+    "<br>",
+    "https://th.bing.com/th/id/R.9a6e33e2ff46107afd15038cc3859993?rik=CHboIf9YAqO4%2fg&pid=ImgRaw&r=0",
+    "<br>",
+    "https://th.bing.com/th/id/OIP.P07q8NgSwaJZOB3emoMmSwHaKV?pid=ImgDet&w=762&h=1063&rs=1",
+    "<br>",
+    "https://th.bing.com/th/id/R.bd55748dfb2ff1952fb8fe215c79b060?rik=iwr3e7e4HvifZQ&pid=ImgRaw&r=0",
+    "<br>",
+    "https://th.bing.com/th/id/R.909f254cb46af93769ac623aa51a01e4?rik=OeO86YiwLdO1JA&pid=ImgRaw&r=0",
+    "<br>",
+    "https://th.bing.com/th/id/R.269dcf52a2af7896a806a3e3413caf6f?rik=FV5GVKsDjg2VHQ&riu=http%3a%2f%2fmarvelll.fr%2fwp-content%2fuploads%2f2013%2f09%2fmarvel-agents-of-shield-poster-saison-1.jpg&ehk=ivoPsztaHPLX8Jee1NwmEJQsVPYxxI2%2f0IcBlId4pD0%3d&risl=&pid=ImgRaw&r=0",
+    "<br>", 
+    "https://pictures.betaseries.com/fonds/poster/f6e33b2e1607c9581d623a317be5aba2.jpg",
+    "<br>",
+    "https://img.ibxk.com.br/2021/11/17/divulgacao-17113639413149.jpg",
+    "<br>",
+
+  ];
+  
+  for (var indice = 0; indice < listaFilmes.length; indice++) {
+    document.write("<img src=" + listaFilmes[indice] + ">");
+  }
+  
+  /* function Adicionar() {
+    var inputAdicionar = document.getElementById("adicionar").value;
+    var aviso = document.getElementById("aviso");
+  
+    if (inputAdicionar == "") {
+      aviso.innerHTML = "Este campo não pode ser vazio";
+    } else if (inputAdicionar == listaFilmes.length) {
+      aviso.innerHTML = "Você já adicionou isso!";
+    } else {
+      aviso.innerHTML = "";
+      listaFilmes.push(inputAdicionar);
+  
+      listaFilmes.innerHTML = listaFilmes + "<img src=" + inputAdicionar + ">";
+    }
+     }
+    */
+  
